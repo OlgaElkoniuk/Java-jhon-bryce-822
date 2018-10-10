669d3a4fa2ed59e350307be33adb505760bc521c
OlgaElkoniuk <elkonuk@gmail.com>
2018-10-10 21:00:48 +0300

    change

--- 10.10.2018/Cinema/src/Program.java
+++ 10.10.2018/Cinema/src/Program.java
@@ -24,19 +24,22 @@ public class Program {
 		}
 		System.out.println("please enter amount of seats you want to buy 1-12>");
 		int amountOsSeats = sc.nextInt();
-		for (amountOsSeats = 1; amountOsSeats <= 12; amountOsSeats++) {
+		for (int count = 1; count <= amountOsSeats; count++) {
 			System.out.println("please enter the number of row 0-3>");
 			int i = sc.nextInt();
 			System.out.println("please enter the number of seat 0-2>");
 			int n = sc.nextInt();
 
 			for (i = 0; i < row.length; i++) {
-
 				for (n = 0; n < row[i].length; n++) {
-					if (row[i][n].available == true) {
-						row[i][n].buySeat();
+					while (row[i][n].available == false) {
+						System.out.println("Sorry, choose another place");
+						break;
 					}
+					row[i][n].buySeat();
+					System.out.println(row[i][n].price);
 				}
+				break;
 			}
 		}
 
